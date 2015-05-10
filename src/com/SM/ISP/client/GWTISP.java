a2e89c545961c6a10f2decf174d6f0260938b9a0
Manny Mengistab <mannymengs@Theaters-MacBook-Pro.local>
2015-05-10 10:50:30 -0400

    first commit

---
+++ src/com/SM/ISP/client/GWTISP.java
@@ -0,0 +1,187 @@
+package com.SM.ISP.client;
+
+import com.SM.ISP.shared.FieldVerifier;
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.event.dom.client.KeyCodes;
+import com.google.gwt.event.dom.client.KeyUpEvent;
+import com.google.gwt.event.dom.client.KeyUpHandler;
+import com.google.gwt.user.client.rpc.AsyncCallback;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.DialogBox;
+import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.PasswordTextBox;
+import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.user.client.ui.TextBox;
+import com.google.gwt.user.client.ui.VerticalPanel;
+
+/**
+ * Entry point classes define <code>onModuleLoad()</code>.
+ */
+public class GWTISP implements EntryPoint {
+	/**
+	 * The message displayed to the user when the server cannot be reached or
+	 * returns an error.
+	 */
+	private static final String SERVER_ERROR = "An error occurred while "
+			+ "attempting to contact the server. Please check your network "
+			+ "connection and try again.";
+
+	/**
+	 * Create a remote service proxy to talk to the server-side Greeting service.
+	 */
+	private final GreetingServiceAsync greetingService = GWT
+			.create(GreetingService.class);
+
+	/**
+	 * This is the entry point method.
+	 */
+	public void onModuleLoad() 
+	{
+		final Button loginButton = new Button("Login");
+		final TextBox usernameField = new TextBox();
+		usernameField.getElement().setPropertyString("placeholder", "username");
+		final TextBox passwordField = new PasswordTextBox();
+		passwordField.getElement().setPropertyString("placeholder", "password");
+		//usernameField.setText("username");
+		//passwordField.setText("password");
+		final Label errorLabel = new Label();
+		final Label responseLabel = new Label();
+
+		// We can add style names to widgets
+		loginButton.addStyleName("sendButton");
+
+		// Add the nameField and sendButton to the RootPanel
+		// Use RootPanel.get() to get the entire body element
+		RootPanel.get().addStyleName("gwt-RootPanel");
+		RootPanel.get("usernameFieldContainer").add(usernameField);
+		RootPanel.get("passwordFieldContainer").add(passwordField);
+		RootPanel.get("sendButtonContainer").add(loginButton);
+		RootPanel.get("responseContainer").add(responseLabel);
+		RootPanel.get("errorLabelContainer").add(errorLabel);
+
+		// Focus the cursor on the name field when the app loads
+		usernameField.setFocus(true);
+		usernameField.selectAll();
+
+		// Create the popup dialog box
+		//final DialogBox dialogBox = new DialogBox();
+		//dialogBox.setText("Remote Procedure Call");
+		//dialogBox.setAnimationEnabled(true);
+		//final Button closeButton = new Button("Close");
+		
+		// We can set the id of a widget by accessing its Element
+		//closeButton.getElement().setId("closeButton");
+		final Label textToServerLabel = new Label();
+		final HTML serverResponseLabel = new HTML();
+		VerticalPanel dialogVPanel = new VerticalPanel();
+		dialogVPanel.addStyleName("dialogVPanel");
+		dialogVPanel.add(new HTML("<b>Sending name to the server:</b>"));
+		dialogVPanel.add(textToServerLabel);
+		dialogVPanel.add(new HTML("<br><b>Server replies:</b>"));
+		dialogVPanel.add(serverResponseLabel);
+		dialogVPanel.setHorizontalAlignment(VerticalPanel.ALIGN_RIGHT);
+		//dialogVPanel.add(closeButton);
+		//dialogBox.setWidget(dialogVPanel);
+		
+		// Add a handler to verify login credentials
+		loginButton.addClickHandler(new ClickHandler() 
+		{
+			public void onClick(ClickEvent event) 
+			{
+				loginButton.setEnabled(true);
+				loginButton.setFocus(true);
+			}
+		});
+
+		// Create a handler for the loginButton, usernameField, and passwordField
+		class MyHandler implements ClickHandler, KeyUpHandler 
+		{
+			/**
+			 * Fired when the user clicks on the loginButton.
+			 */
+			public void onClick(ClickEvent event) 
+			{
+				sendNameToServer();
+			}
+
+			/**
+			 * Fired when the user types in the usernameField and password fields.
+			 */
+			public void onKeyUp(KeyUpEvent event) 
+			{
+				if (event.getNativeKeyCode() == KeyCodes.KEY_ENTER) 
+				{
+					sendNameToServer();
+				}
+			}
+
+			/**
+			 * Send the name from the nameField to the server and wait for a response.
+			 */
+			private void sendNameToServer() 
+			{
+	
+				// First, we validate the input by querying RL server
+				errorLabel.setText("");
+				String usernameToServer = usernameField.getText();
+				String passwordToServer = passwordField.getText();
+				if (!FieldVerifier.isValidLogin(usernameToServer, passwordToServer)) 
+				{
+					errorLabel.setText("INVALID USERNAME OR PASSWORD");
+					return;
+				}
+				else
+				{
+					responseLabel.setText("Loading...");
+				}
+
+				// Then, we send the input to the server.
+				loginButton.setEnabled(false);
+				textToServerLabel.setText(usernameToServer);
+				serverResponseLabel.setText("");
+				greetingService.greetServer(usernameToServer,
+						new AsyncCallback<String>() 
+						{
+							public void onFailure(Throwable caught) 
+							{
+								// Show the RPC error message to the user
+								/*
+								dialogBox
+										.setText("Remote Procedure Call - Failure");
+								serverResponseLabel
+										.addStyleName("serverResponseLabelError");
+								serverResponseLabel.setHTML(SERVER_ERROR);
+								dialogBox.center();
+								closeButton.setFocus(true);
+								*/
+								//Inform of failure and re-prompt
+								responseLabel.setText("Try again");
+							}
+
+							public void onSuccess(String result) 
+							{
+								/*
+								dialogBox.setText("Remote Procedure Call");
+								serverResponseLabel
+										.removeStyleName("serverResponseLabelError");
+								serverResponseLabel.setHTML(result);
+								dialogBox.center();
+								closeButton.setFocus(true);
+								*/
+								//Change webpage
+							}
+						});
+			}
+		}
+
+		// Add a handler to send the name to the server
+		MyHandler handler = new MyHandler();
+		loginButton.addClickHandler(handler);
+		usernameField.addKeyUpHandler(handler);
+		passwordField.addKeyUpHandler(handler);
+	}
+}
---
+++ src/com/SM/ISP/shared/ISP.java
@@ -0,0 +1,352 @@
+package com.SM.ISP.shared;
+import javax.swing.text.*;
+import java.util.*;
+
+/**
+ * 
+ * @author mannymengs
+ * This class simulates an ISP entry in the database.
+ */
+
+public class ISP
+{
+	protected static final ArrayList<String> topics = new ArrayList<String>();
+	private int ID;
+	private String sf, sl, ct, cf, cl, o1, o2, oA, oC, oS, oZ, an, ce, cp, a, r;
+	private String t;
+	
+	/**
+	 * Constructor for ISP object
+	 * @param id the ID number of the ISP (joint ISPs have the same number)
+	 * @param topic the topic of the ISP
+	 * @param studentFirst the first name of the student
+	 * @param studentLast the last name of the student
+	 * @param contactTitle the title of the contact (Mr., Dr., Prof., etc.)
+	 * @param contactFirst the first name of the contact
+	 * @param contactLast the last name of the contact
+	 * @param orgLine1 the name of the first organization with which the student is involved
+	 * @param orgLine2 the name of the second organization with which the student is involved
+	 * @param orgAddress the address of the primary organization
+	 * @param orgCity the city in which the primary organization is located
+	 * @param orgState the state in which the primary organization is located
+	 * @param orgZip the zip code of the primary organization
+	 * @param advisorName the name of the advisor
+	 * @param contactEmail the email of the advisor
+	 * @param contactPhone the phone number of the advisor
+	 * @param abstraction the abstract written by the student
+	 * @param reflection the reflection written by the student
+	 */
+	public ISP(int id, String topic, String studentFirst, String studentLast, 
+			String contactTitle, String contactFirst, String contactLast, 
+			String orgLine1, String orgLine2, 
+			String orgAddress, String orgCity, String orgState, String orgZip,
+			String advisorName, String contactEmail, String contactPhone,
+			String abstraction, String reflection)
+	{
+		ID = id;
+		t = topic;
+		sf = studentFirst;
+		sl = studentLast;
+		ct = contactTitle;
+		cf = contactFirst;
+		cl = contactLast;
+		o1 = orgLine1;
+		o2 = orgLine2;
+		oA = orgAddress;
+		oC = orgCity;
+		oS = orgState;
+		oZ = orgZip;
+		an = advisorName;
+		ce = contactEmail;
+		cp = contactPhone;
+		a = abstraction;
+		r = reflection;
+		if(!topics.contains(t))
+		{
+			topics.add(t);
+			Collections.sort(topics);
+		}
+	}
+	
+	/**
+	 * This method formats the ISP entry into a string which can be displayed on the page
+	 * @return display the display string holding the formatted ISP entry
+	 */
+	public String toString()
+	{
+		String display = "";
+		display += "Student: " + sf + " " + sl + "\n";
+		display += "Topic: " + t + "\n";
+		display += "Primary Organization: " + o1 + "\n";
+		if(o2 != null || o2.length() > 0)
+			display += "Secondary Organization: " + o2 + "\n";
+		display += "Address: " + oA + "\n" + oC + ", " + oS + " " + oZ + "\n";
+		display += "On-Campus Advisor: " + an + "\n";
+		display += "Off-Campus Advisor: " + ct + " " + cf + " " + cl + "\n";
+		display += "Off-Campus Advisor Email: " + ce + "\n";
+		display += "Off-Campus Advisor Phone #: " + cp + "\n";
+		display += "Abstract:\n" + a + "\n";
+		display += "Reflection:\n" + r + "\n";
+		return display;
+	}
+
+	/**
+	 * @return the ID
+	 */
+	public int getID() {
+		return ID;
+	}
+
+	/**
+	 * @param ID the ID to set
+	 */
+	public void setID(int ID) {
+		this.ID = ID;
+	}
+
+	/**
+	 * @return the sf
+	 */
+	public String getSf() {
+		return sf;
+	}
+
+	/**
+	 * @param sf the sf to set
+	 */
+	public void setSf(String sf) {
+		this.sf = sf;
+	}
+
+	/**
+	 * @return the sl
+	 */
+	public String getSl() {
+		return sl;
+	}
+
+	/**
+	 * @param sl the sl to set
+	 */
+	public void setSl(String sl) {
+		this.sl = sl;
+	}
+
+	/**
+	 * @return the ct
+	 */
+	public String getCt() {
+		return ct;
+	}
+
+	/**
+	 * @param ct the ct to set
+	 */
+	public void setCt(String ct) {
+		this.ct = ct;
+	}
+
+	/**
+	 * @return the cf
+	 */
+	public String getCf() {
+		return cf;
+	}
+
+	/**
+	 * @param cf the cf to set
+	 */
+	public void setCf(String cf) {
+		this.cf = cf;
+	}
+
+	/**
+	 * @return the cl
+	 */
+	public String getCl() {
+		return cl;
+	}
+
+	/**
+	 * @param cl the cl to set
+	 */
+	public void setCl(String cl) {
+		this.cl = cl;
+	}
+
+	/**
+	 * @return the o1
+	 */
+	public String getO1() {
+		return o1;
+	}
+
+	/**
+	 * @param o1 the o1 to set
+	 */
+	public void setO1(String o1) {
+		this.o1 = o1;
+	}
+
+	/**
+	 * @return the o2
+	 */
+	public String getO2() {
+		return o2;
+	}
+
+	/**
+	 * @param o2 the o2 to set
+	 */
+	public void setO2(String o2) {
+		this.o2 = o2;
+	}
+
+	/**
+	 * @return the oA
+	 */
+	public String getoA() {
+		return oA;
+	}
+
+	/**
+	 * @param oA the oA to set
+	 */
+	public void setoA(String oA) {
+		this.oA = oA;
+	}
+
+	/**
+	 * @return the oC
+	 */
+	public String getoC() {
+		return oC;
+	}
+
+	/**
+	 * @param oC the oC to set
+	 */
+	public void setoC(String oC) {
+		this.oC = oC;
+	}
+
+	/**
+	 * @return the oS
+	 */
+	public String getoS() {
+		return oS;
+	}
+
+	/**
+	 * @param oS the oS to set
+	 */
+	public void setoS(String oS) {
+		this.oS = oS;
+	}
+
+	/**
+	 * @return the oZ
+	 */
+	public String getoZ() {
+		return oZ;
+	}
+
+	/**
+	 * @param oZ the oZ to set
+	 */
+	public void setoZ(String oZ) {
+		this.oZ = oZ;
+	}
+
+	/**
+	 * @return the an
+	 */
+	public String getAn() {
+		return an;
+	}
+
+	/**
+	 * @param an the an to set
+	 */
+	public void setAn(String an) {
+		this.an = an;
+	}
+
+	/**
+	 * @return the ce
+	 */
+	public String getCe() {
+		return ce;
+	}
+
+	/**
+	 * @param ce the ce to set
+	 */
+	public void setCe(String ce) {
+		this.ce = ce;
+	}
+
+	/**
+	 * @return the cp
+	 */
+	public String getCp() {
+		return cp;
+	}
+
+	/**
+	 * @param cp the cp to set
+	 */
+	public void setCp(String cp) {
+		this.cp = cp;
+	}
+
+	/**
+	 * @return the a
+	 */
+	public String getA() {
+		return a;
+	}
+
+	/**
+	 * @param a the a to set
+	 */
+	public void setA(String a) {
+		this.a = a;
+	}
+
+	/**
+	 * @return the r
+	 */
+	public String getR() {
+		return r;
+	}
+
+	/**
+	 * @param r the r to set
+	 */
+	public void setR(String r) {
+		this.r = r;
+	}
+
+	/**
+	 * @return the t
+	 */
+	public String getT() {
+		return t;
+	}
+
+	/**
+	 * @param t the t to set
+	 */
+	public void setT(String t) {
+		this.t = t;
+	}
+
+	/**
+	 * @return the topics
+	 */
+	public static ArrayList<String> getTopics() {
+		return topics;
+	}
+	
+}
